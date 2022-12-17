19fa742803d0dade0a4b363f360c84610a682ba6
aakash <aakashinthesky@gmail.com>
2022-12-16 20:58:35 -0800

    Added placeholder code for tain loop

--- train.py
+++ train.py
@@ -5,8 +5,20 @@ from t5x.partitioning import PjitPartitioner
 from palm_main import PaLM
 from config import PaLMConfig
 
+from torch.utils.data import DataLoader, Dataset
+
+import tqdm
+import wandb
+
+import jax.numpy as jnp
+
 def train():
     config = PaLMConfig
     model = PaLM(config = config)
+    pb = tqdm.tqdm()
     
-    
+    while True:
+        seq = jnp.ones((16, 2049), dtype=jnp.uint)
+        metrics = model.train_step(seq)
+        pb.update(1)
+        # wandb.log(metrics)
