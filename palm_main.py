38f321afb210fa9731d28800a028f5802380ce2c
TheRealAakash <aakashinthesky@gmail.com>
2022-12-17 16:27:02 -0800

    Update

--- palm_main.py
+++ palm_main.py
@@ -130,7 +130,7 @@ class PaLM:
                 model_state_spec, 
                 P("data",)
             ),
-            out_axis_resources=(model_state_spec, P("data",))
+            out_axis_resources=(model_state_spec, None)
         )
         
         n_params_flax = sum(
