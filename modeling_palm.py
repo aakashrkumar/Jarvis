7331c1b42046898ea46c54dd8a8d3058f73a4ef9
TheRealAakash <aakashinthesky@gmail.com>
2023-02-10 15:34:41 -0800

    Update

--- modeling_palm.py
+++ modeling_palm.py
@@ -79,9 +79,9 @@ class ParallelTransformerBlock(nn.Module):
         
         q, k, v, ff = jnp.split(fused_attn_ff_proj, split_indices, axis = -1)
         q = with_sharding_constraint(q, ("batch", "length", "heads", "kv"))
-        k = with_sharding_constraint(q, ("batch", "length", "kv"))
-        v = with_sharding_constraint(q, ("batch", "length", "kv"))
-        f = with_sharding_constraint(q, ("batch", "length", "mlp"))
+        k = with_sharding_constraint(k, ("batch", "length", "kv"))
+        v = with_sharding_constraint(v, ("batch", "length", "kv"))
+        ff = with_sharding_constraint(ff, ("batch", "length", "mlp"))
         # split heads
         # they use multi-query single-key-value attention, yet another Noam Shazeer paper
         # they found no performance loss past a certain scale, and more efficient decoding obviously
